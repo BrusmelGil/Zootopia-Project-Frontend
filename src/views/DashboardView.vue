c8c36515f81fde422d5947fc09f35a3b4d7857d4
Mark-Kil <markkilcanarias@gmail.com>
2024-01-15 13:57:57 +0100

    project prepared

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>This is a dashboard view,</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1>This is login page</h1>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
