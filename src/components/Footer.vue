3605073591fb15a0d2f710c1784c6cbfcc550623
Vero-Sampedro <verosampedro@icloud.com>
2024-01-16 15:36:02 +0100

    footer design done

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+
+<div id="rrss">
+
+    <img src="/facebook-svgrepo-com.svg" alt="">
+    <img src="/instagram-1-svgrepo-com.svg" alt="">
+    <img src="/twitter-svgrepo-com.svg" alt="">
+
+</div>
+
+</template>
+
+<style lang="scss" scoped>
+
+#rrss {
+    display: flex;
+    justify-content: flex-end;
+
+    background-color: $dark-green;
+
+    img {
+        width: 3%;
+        padding: 10px;
+    }
+}
+
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
 </script>
 
 <template>
   <div>
     <Header />
     <h1>This is a dashboard view,</h1>
+    <Footer />
   </div>
 </template>
 
