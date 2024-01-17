a0734d3f7c265bc0862f62f3916942c4d5b017d8
Brusmel Gil <Brusmel@outlook.es>
2024-01-17 20:59:56 +0100

    guardado

---
+++ src/components/cards/Cards.vue
@@ -0,0 +1,133 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+
+    <div class="container">
+        <div class="image">
+        <img src="../../../public/img/pexels-oleksandr-p-1004517 2.png" alt="Imagen de ave">
+        </div>
+
+        <div class="input-global">
+        <div class="form-group">
+            <label for="family">Family: </label>
+            <input type="text" id="family" placeholder="Aves">
+        </div>
+
+        <div class="form-group">
+            <label for="type">Type: </label>
+            <input type="text" id="type" placeholder="Loro de Cacatua">
+            </div>
+
+            <div class="form-group">
+            <label for="gender">Gender: </label>
+            <input type="text" id="gender" placeholder="Macho">
+            </div>
+
+            <div class="form-group">
+            <label for="admission-date">Date of admission: </label>
+            <input type="date" id="admission-date">
+            </div>
+
+            <div class="buttons-container">
+            <div class="btn"><button type="submit">Editar</button></div>
+            <div class="btn"><button type="submit">Borrar</button></div>
+        </div>
+        </div>
+    </div>
+    
+
+    <div class="container">
+        <div class="image">
+        <img src="../../../public/img/pexels-pixabay-2361.png" alt="Imagen de ave">
+        </div>
+
+        <div class="input-global">
+        <div class="form-group">
+            <label for="family">Family: </label>
+            <input type="text" id="family" placeholder="Caninos">
+        </div>
+
+        <div class="form-group">
+            <label for="type">Type: </label>
+            <input type="text" id="type" placeholder="Lobo">
+            </div>
+
+            <div class="form-group">
+            <label for="gender">Gender: </label>
+            <input type="text" id="gender" placeholder="Macho">
+            </div>
+
+            <div class="form-group">
+            <label for="admission-date">Date of admission: </label>
+            <input type="date" id="admission-date">
+            </div>
+
+            <div class="buttons-container">
+            <div class="btn"><button type="submit">Editar</button></div>
+            <div class="btn"><button type="submit">Borrar</button></div>
+        </div>
+        </div>
+    </div>
+
+    
+
+
+
+</template>
+
+<style lang="scss" scoped>
+
+.container{
+    display: flex;
+    border: 1px solid black;
+    width: 35%;
+    height: 50%;
+    border-radius: 15px ;
+
+
+}
+
+#miInput{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.image img{
+    width: 75%;
+    margin-left: 5%;
+    margin-top: 20px;
+
+}
+
+.input-global{
+    margin: 5% 0;
+}
+
+
+    .btn {
+    margin-right: 0px; 
+    display: inline;
+    justify-content:space-between ;
+}
+
+.btn:last-child {
+    margin-right: 0;
+}
+
+    
+
+
+
+    
+
+
+
+
+
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,13 +1,25 @@
+
+
 <script setup>
+import Cards from '@/components/cards/Cards.vue';
+
 
 </script>
 
 <template>
   <div>
-    <h1>This is a dashboard view,</h1>
+    <Cards />
+    <!-- <h1>This is a dashboard view,</h1> -->
   </div>
 </template>
 
 <style lang="scss" scoped>
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+}
 
 </style>
