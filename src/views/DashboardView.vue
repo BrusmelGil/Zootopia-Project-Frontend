d6db9a977561817f1a2800aae02515f3900576ef
Vero-Sampedro <verosampedro@icloud.com>
2024-01-18 14:27:19 +0100

    searchbar design done

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from '../components/Header.vue';
 import Footer from '../components/Footer.vue';
+import Searchbar from '../components/buttons/Searchbar.vue';
 </script>
 
 <template>
   <div>
     <Header />
+    <Searchbar />
     <h1>This is a dashboard view,</h1>
     <Footer />
   </div>
