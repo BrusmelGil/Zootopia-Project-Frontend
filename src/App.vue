06e3a994601f4876c2791798e332b9a6a1e96aa2
Rominaruizdiaz <ruizdiazsanchezromina@gmail.com>
2024-01-22 15:25:18 +0100

    added login interface

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,12 @@ import { RouterLink, RouterView } from "vue-router";
 
 <template>
   <header>
-    <!-- <div class="wrapper">
-
+    <div class="wrapper">
       <nav>
         <RouterLink to="/">Login</RouterLink>
         <RouterLink to="/dashboard">Dashboard</RouterLink>
       </nav>
-    </div> -->
+    </div>
   </header>
 
   <RouterView />
